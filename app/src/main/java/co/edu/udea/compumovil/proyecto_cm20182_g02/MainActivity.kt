349444790fd7309fa380987eaef56bc17ecf5fff
EDWARMARTINEZ <edwar.martinez@udea.edu.co>
2018-09-05 15:26:22 -0500

    1. Aplication Icono 2. Presentacion Splash Screen

--- app/src/main/java/co/edu/udea/compumovil/proyecto_cm20182_g02/MainActivity.kt
+++ app/src/main/java/co/edu/udea/compumovil/proyecto_cm20182_g02/MainActivity.kt
@@ -1,12 +1,25 @@
 package co.edu.udea.compumovil.proyecto_cm20182_g02
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        Handler().postDelayed(Runnable { cargarActivityLoguin() }, 3000)
+
+    }
+
+
+    private fun cargarActivityLoguin() {
+        val miIntent:Intent= Intent ( this, LoginActivity::class.java)
+            startActivity(miIntent)
+            finish()
     }
 }
