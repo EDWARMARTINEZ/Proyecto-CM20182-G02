beace65b7bca4f4e95055b9e693ecd8281a96dca
EDWARMARTINEZ <edwar.martinez@udea.edu.co>
2018-09-05 12:10:51 -0500

    1. Gestionar Activitys 2. Factory rules

---
+++ app/src/androidTest/java/co/edu/udea/compumovil/proyecto_cm20182_g02/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package co.edu.udea.compumovil.proyecto_cm20182_g02
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("co.edu.udea.compumovil.proyecto_cm20182_g02", appContext.packageName)
+    }
+}
