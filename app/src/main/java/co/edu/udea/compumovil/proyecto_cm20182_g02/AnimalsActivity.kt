69aaa93dfa33f6f2a14a698eef452a6816b1353d
gonzalo-villegas <chalo1970@gmail.com>
2018-09-05 16:24:40 -0500

    Gestion de Activitys 1 - Activity Menu 2 - Activity Inventory 3 - Activity Supplies 4 - Activity Animals 5 - Activity Breeding 6 - Activity Diary 7 - Activity Producto

---
+++ app/src/main/java/co/edu/udea/compumovil/proyecto_cm20182_g02/AnimalsActivity.kt
@@ -0,0 +1,12 @@
+package co.edu.udea.compumovil.proyecto_cm20182_g02
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AnimalsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_animals)
+    }
+}
